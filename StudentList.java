7947e3efcda4bb27800138ffb07ef7ad858f306f
790346 <790346@seq.org>
2016-11-09 08:46:02 -0800

    Add files via upload

---
+++ StudentList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentList
+{
+    Student[] students = new Student[2];
+    ArrayList<Student> studs = new ArrayList<Student>();
+    public void addStudent(Student s, int index)
+    {
+        s.getStudent();
+        
+        s = students[index];
+       
+    }
+}
